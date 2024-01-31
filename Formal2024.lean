cd76072c91028a75face5a7a685193d6a173d53b
themathqueen <23701951+themathqueen@users.noreply.github.com>
2024-01-31 16:26:07 +0000

    initial files

--- Formal2024.lean
+++ Formal2024.lean
@@ -1,2 +1,4 @@
 import Formal2024.Homeworks.homework1
 import Formal2024.Workshops.workshop1
+import Formal2024.Homeworks.homework2
+import Formal2024.Workshops.workshop2
---
+++ Formal2024/Workshops/workshop2.lean
@@ -0,0 +1,5 @@
+/-
+  # Logic with Lean: session 2
+  ## how to use `→`, `∀` and `∃` with Lean
+  References: [MIL] Mathematics in Lean, [Tut] Tutorials project.
+-/
