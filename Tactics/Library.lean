bff4fc70371da1524ff69ca07d6e1fb94293ef74
Adrián Doña Mateo <43966663+AdrianDoM@users.noreply.github.com>
2024-02-16 23:13:08 +0000

    Ignore Paperproof

--- Tactics/Library.lean
+++ Tactics/Library.lean
@@ -3,11 +3,12 @@ Copyright (c) 2024 TheLeanTeam. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: The Lean Team
 -/
-import Paperproof
 import Aesop
-import Tactics.TruthTable
+import TruthTable
 import AutograderLib
 import Mathlib.Tactic
+-- import Paperproof
+
 
 open Lean Elab Tactic
 
