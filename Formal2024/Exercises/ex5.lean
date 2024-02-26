e0e90a60040835f19cce5fd8c82a7ba771e2484d
Adrián Doña Mateo <drnaia100@gmail.com>
2024-02-26 14:35:37 +0000

    Add comments to exercises

--- Formal2024/Exercises/ex5.lean
+++ Formal2024/Exercises/ex5.lean
@@ -43,60 +43,122 @@ theorem myEven_pow : ∀ m n, myEven m → myEven (m ^ n) := sorry
 
 theorem myOdd_pow : ∀ m n, myOdd m → myOdd (m ^ n) := sorry
 
-/-! # Part 2 : uniqueness of natural numbers with `≤`
 
-  `∃! x` is notation for "there exists a unique `x`".
+/-! # Part 2 : Unique existence
+  We introduce the useful `∃!` notation. Given a type `α` and `p : α → Prop`, `∃! x : α, p x` is
+  a proposition asserting that there exists a unique term `x : α` such that `p x` holds.
   This is equivalent to `∃ x, p x ∧ ∀ y, p y → y = x`.
-
-  This can be confusing when involving more variables, so we will prove the following.
 -/
 
-theorem Nat.exists_unique_le : ∃! n m : Nat, m ≤ n := sorry
+-- Examine the following example to see how `∃!` is defined; then try to prove it.
+example : ∃! x : ℕ, x = 0 := by
+  dsimp [ExistsUnique]
+  sorry
 
-/-! # Part 3 : existence implies everything (if and only if the universe is nonempty)
-  in other words, if we know something exists, then everything exists -/
+-- The `∃!` notation can take several variables, but this can be quite confusing.
+-- For example, try to understand what the following lemma is saying, and then prove it.
+lemma Nat.exists_unique_le : ∃! n m : Nat, m ≤ n := by
+  sorry
 
-theorem existence_implies_everything_iff {α : Type*} (r : α → Prop) :
+
+/-
+  ## Part 3 : Types and logic
+  Here we examine some interesting interactions between types and logic.
+-/
+
+-- Lean has an inductive type (class) called `Nonempty`. It represents when a type has at least
+-- one term. You can `ctrl/cmd + click` on `Nonempty` to see its definition.
+#check Nonempty
+
+-- Given that it is an inductive type, do you see how you could use and construct a term of
+-- type `Nonempty α` for some type `α`? Try the following:
+
+example : Nonempty ℕ := sorry
+
+example {α β : Type} : Nonempty α → Nonempty β → Nonempty (α × β) := sorry
+
+-- Now try to understand and prove the following lemma.
+lemma existence_implies_everything_iff {α : Type*} (r : α → Prop) :
   (∃ x, (r x → ∀ y, r y)) ↔ Nonempty α := sorry
 
-section Part_4
-/-! # Part 4 : non-equal types
-
-Firstly, some definitions:
-* `Cardinal` is a structure that represents the cardinality of a type.
-  We can use it to compare the cardinality of two types.
-  In order to use the notation `#α`, which exactly means "the cardinality of `α`", we need to `open Cardinal` first.
-* `Countable α` is defined as `Prop`, which is determined by whether there exists an injective map `α → ℕ`.
-* `Uncountable α` is defined as `¬ (Countable α)`.
+-- You can think of `Nonempty` as a special case of `Exists` (`∃`), where the predicate is trivial.
 
 
-We will prove the following lemmas and theorems about non-equal types.
-
-* `ne_types_of_ne_cardinal`: we have non-equal types given their cardinality is non-equal
-
-* `ne_types_of_countable_uncountable`: if a type is countable and the other isn't, then they are non-equal
-
-* `ne_types_of_different_property`: generalising the above two,
-    we get non-equal types given there is a different property
-
-an easy example of all of this is the following.
-* `exists_ne_types`: there exists two types that are non-equal
+/-
+  ## Part 4 : Type equality and cardinality
+  For this question, we will explore when two types can be the same in two different ways.
+  One of them is equality, which is very strict. Another is having the same cardinality.
 -/
 
+-- Two types being equal is a very strong statement. For example, if `α` and `β` are equal, then
+-- any term of type `α` can be used as a term of type `β`.
+
+-- Conversely, if `x : α` and `x : β` then `α = β`. This is not something we can prove,
+-- it is a syntactical necessity.
+
+-- We can however prove that two types are not equal, if they satisfy different properties.
+lemma ne_types_of_different_property {α β : Type}
+  (p : Type → Prop) (hα : p α) (hβ : ¬ p β) : α ≠ β := sorry
+
+-- Now prove that there exists two types that are not equal.
+lemma exists_ne_types : ∃ α β : Type, α ≠ β := sorry
+-- If you get stuck at this point, skip this exercise and come back to it at the end!
+
+section
+
+/-
+  Types can be related in other ways other than being equal. For example, mathlib defines
+  a type `Cardinal` which is a quotient of `Type` by the equivalence relation of having the same
+  cardinality. Two types have the same cardinality if there exists a bijection between them.
+-/
+
+-- `ctrl/cmd + click` through to examine the definitions.
+#check Cardinal
+-- Note how `Nonempty` is used in the definition of `Cardinal`.
+
+-- There are also types that witness when a type is countable or uncountable.
+#check Countable
+#check Uncountable
+-- These are defined as classes, which we haven't seen yet, but you can just think of them as
+-- inductive types with only one constructor for now.
+
+-- Mathlib defines the convenient notation `#α` for the cardinal corresponding to a type `α`.
+-- To access it `#` we need to open the `Cardinal` namespace.
 open Cardinal
 
-lemma ne_types_of_ne_cardinal {α β : Type} (h : #α ≠ #β) : α ≠ β := sorry
+section
+variable {α : Type}
 
+#check #α
+-- This is the same as `Quotient.mk Cardinal.isEquivalent α`.
+end
+
+-- Now prove that two types with different cardinality cannot be equal.
+lemma ne_types_of_ne_cardinal {α β : Type} (h : #α ≠ #β) : α ≠ β := by
+  sorry
+
+-- Now prove this. (Hint: you don't need to use the previous one.)
 lemma ne_types_of_countable_uncountable {α β : Type}
   (hα : Countable α) (hβ : Uncountable β) : α ≠ β := sorry
 
-theorem ne_types_of_different_property {α β : Type}
-  (p : Type → Prop) (hα : p α) (hβ : ¬ p β) : α ≠ β := sorry
+-- *Reminder*: if you couldn't find two types that were not equal before, go back a try it now.
 
-example : ∃ α β : Type, α ≠ β := sorry
+end
 
-end Part_4
 
-/-! # Part 5 : Nat.cast is not surjective -/
+/-
+  ## Q4: Nat.cast is not surjective
+  For this last question, we will prove that the obvious inclusion `ℕ → ℤ` is not surjective.
+  In Lean, this inclusion is called `Nat.cast`.
+-/
 
-theorem Nat.cast_is_not_surjective : ¬ Function.Surjective (Nat.cast : ℕ → ℤ) := sorry
+#check Nat.cast (R := ℤ)
+-- Cast is defined in greater generality as a function `ℕ → R` for certain types `R`. This
+-- argument is implicit, but we can provide it explicilty using the `(R := ℤ)` syntax.
+#check Nat.cast (R := ℤ) 0
+-- Note that Lean uses the `↑` notation for `Nat.cast`. We will learn more about this next week.
+
+lemma not_surjective_nat_int : ¬ Function.Surjective (Nat.cast : ℕ → ℤ) := sorry
+
+-- You may find the following lemma useful.
+#check Nat.cast_lt (α := ℤ)
